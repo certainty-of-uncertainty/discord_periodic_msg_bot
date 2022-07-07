e7b2d79a832ef1c2c50d6137e9cec5bcebe26113
eccentricmiked <24943033+eccentricmiked@users.noreply.github.com>
2022-07-07 03:25:48 +0000

    more testing

--- send_daily_game_messages.py
+++ send_daily_game_messages.py
@@ -11,10 +11,10 @@ class MyClient(commands.Bot):
 
   async def on_ready(self):
     print('We have logged in as {0.user}'.format(client))
-    self.SendMessageToRpgBot.start()
-    time.sleep(10)
-    self.SendMessageToMee6Bot.start()
-    time.sleep(10)
+    #self.SendMessageToRpgBot.start()
+    #time.sleep(10)
+    #self.SendMessageToMee6Bot.start()
+    #time.sleep(10)
     self.SendMessageToFortniteBot.start()
     time.sleep(10)
 
@@ -22,30 +22,32 @@ class MyClient(commands.Bot):
   async def SendMessageToRpgBot(self):
     channel = self.get_channel(int(os.environ['RPG_Channel_ID']))
     
-    message = await channel.send("rpg daily")
+    message = await channel.send("rpg daily", delete_after=60)
     await self.process_commands(message)
     time.sleep(5)
-    message = await channel.send("rpg weekly")
+    message = await channel.send("rpg weekly", delete_after=60)
     await self.process_commands(message)
     time.sleep(5)
 
   @tasks.loop(minutes=1)
   async def SendMessageToMee6Bot(self):
     channel = self.get_channel(int(os.environ['MEE6_Game_Channel_ID']))
-    message = await channel.send("/daily")
+    message = await channel.send("/daily", delete_after=60)
     await self.process_commands(message)
     time.sleep(5)
-    message = await channel.send("/weekly")
+    message = await channel.send("/weekly", delete_after=60)
     await self.process_commands(message)
     time.sleep(5)
   
   @tasks.loop(minutes=1)
   async def SendMessageToFortniteBot(self):
     channel = self.get_channel(int(os.environ['Fortnite_Game_Channel_ID']))
-    message = await channel.send("/claim-daily")
+    await channel.trigger_typing()
+    message = await channel.send("/claim-daily", delete_after=60)
     await self.process_commands(message)
     time.sleep(10)
-    message = await channel.send("/research collect")
+    await channel.trigger_typing()
+    message = await channel.send("/research collect", delete_after=60)
     await self.process_commands(message)
     time.sleep(10)
 
